5173b8bcd64bf255c69ff8e9d4e2cbd3022e76d0
parthb83 <parthbansal1536@gmail.com>
2021-11-16 21:21:26 +0530

    Added models to save messages

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,6 +1,7 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 from asgiref.sync import sync_to_async
+from .models import Message
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
@@ -26,6 +27,8 @@ class ChatConsumer(AsyncWebsocketConsumer):
         username = data['username']
         room = data['room']
 
+        await self.save_message(username, room, message)
+
         await self.channel_layer.group_send(
             self.room_group_name,
             {
@@ -43,3 +46,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
             'message': message,
             'username': username
         }))
+
+    @sync_to_async
+    def save_message(self, username, room, message):
+        Message.objects.create(username=username, room=room, content=message)
--- chat/views.py
+++ chat/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Message
 
 def index(request):
     return render(request, 'index.html')
@@ -6,5 +7,6 @@ def index(request):
 
 def room(request, room_name):
     username = request.GET.get('username', 'Anonymous')
+    messages = Message.objects.filter(room=room_name)[0:25]
 
-    return render(request, 'room.html', {'room_name': room_name, 'username': username})
+    return render(request, 'room.html', {'room_name': room_name, 'username': username, 'messages': messages})
